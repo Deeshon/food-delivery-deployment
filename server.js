358ce9d685ab4e36523e584535104c8714f094db
Deeshon Hunukumbura <deeshonhunukumbura18@gmail.com>
2023-06-05 00:15:11 +0530

    final code x2

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use('/uploads', express.static(__dirname + '/uploads'))
 
 app.use("/api", apiRouter)
 
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
 
